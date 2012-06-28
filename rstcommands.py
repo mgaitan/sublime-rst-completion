705785bdfea1c9320bb7f1028019d5c0b7c075d0
Corey Oordt <coreyoordt@gmail.com>
2012-06-28 08:43:33 -0500

    Added some commands and keybindings for doing styling

---
+++ rstcommands.py
@@ -0,0 +1,29 @@
+import sublime, sublime_plugin
+
+
+class SurroundCommand(sublime_plugin.TextCommand):
+    """
+    Base class to surround the selection with text.
+    """
+    surround = ''
+
+    def run(self, edit):
+        for sel in self.view.sel():
+            replacement = "%s%s%s" % (self.surround, self.view.substr(sel), self.surround)
+            self.view.replace(edit, sel, replacement)
+
+
+class StrongEmphasisCommand(SurroundCommand):
+    surround = "**"
+
+
+class EmphasisCommand(SurroundCommand):
+    surround = "*"
+
+
+class LiteralCommand(SurroundCommand):
+    surround = "``"
+
+
+class SubstitutionCommand(SurroundCommand):
+    surround = "|"
