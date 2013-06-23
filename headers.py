ef332997cdf81ad938dbf549a40bb637051dda97
Martín Gaitán <mgaitan@machinalis.com>
2013-06-23 02:15:42 -0300

    doing header autocompletion and folding play well together

--- headers.py
+++ headers.py
@@ -12,12 +12,17 @@ class SmartHeaderCommand(BaseBlockCommand):
                     head_lines == 3 and lines[-3][0] != adornment_char):
                 # invalid header
                 return
-
             title = lines[-2]
             title_lenght = len(title)
+            # is header completed ?
+            if (len(lines[-1]) >= len(title) and
+                    (head_lines == 2 or len(lines[-3]) >= len(title))):
+                self.view.run_command('smart_folding')
+                return
+
             strike = adornment_char * title_lenght
             if head_lines == 2:
-                result = title + '\n' + strike
+                result = title + '\n' + strike + '\n'
             else:
-                result = strike + '\n' + title + '\n' + strike
+                result = strike + '\n' + title + '\n' + strike + '\n'
             self.view.replace(edit, region, result)
